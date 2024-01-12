4e7e2a43507dca069740dea2816a38b57a1b3649
franciscomaciel <francisco.maciel@gmail.com>
2024-01-12 13:33:33 -0300

    Created the apps for documents, companies and overtime records.

--- apps/companies/models.py
+++ apps/companies/models.py
@@ -3,3 +3,7 @@ from django.db import models
 
 class Company(models.Model):
     name = models.CharField(max_length=100, help_text='Company name')
+
+    def __str__(self):
+        return self.name
+
